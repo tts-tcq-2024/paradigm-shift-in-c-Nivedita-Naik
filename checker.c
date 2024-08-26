b6c17bce9150aff9c3ebf94c3c70175eb842da5e
Nivedita Naik <116743692+Nivedita-Naik@users.noreply.github.com>
2024-08-26 20:52:13 +0530

    Update checker1.c

--- checker.c
+++ checker.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 #include <assert.h>
 
-int checkingInRange(float value, float min, float max, const char* parameterName)
-{
-   if(value<min || value>max)
-   {
+int checkingInRange(float value, float min, float max, const char* parameterName){
+   if(value<min || value>max){
        printf("%s out of range\n, parameterName);
-       return 0;
-   }
+       return 0;}
    return 1;
 }
 
-int batteryIsOk(float temperature, float soc, float chargeRate)
-{
+int batteryIsOk(float temperature, float soc, float chargeRate){
     return checkingInRange(temperature, 0, 45, "Temperature")&&
            checkingInRange(soc,20,80, "State of charge")&&
            checkingInRange(chargeRate,0,0.8,"Charge Rate");
